85d941be4726fec45252961cf4296590a5ecf27f
Henry Nguyen <henry2833@gmail.com>
2021-03-24 11:56:58 -0700

    sources filter fixes

--- assets/make_cyto_graph.mjs
+++ assets/make_cyto_graph.mjs
@@ -12,28 +12,12 @@ function save_func(personal_value, storage_index = 1) {
 var filter_exported = {
     apply_no_long_descs: function () {
         cy.$('.problem-view').removeClass('problem-view')
-        cy.nodes('node[properties].personal-value,.risk-solution').forEach(function (node) {
-            if (node.data('properties')['schema_longDescription'].length === 0) {
-                node.addClass('problem-view')
-            }
-        })
+        cy.nodes('.no-long-description').addClass('problem-view')
     },
     apply_no_sources: function () {
         cy.$('.problem-view').removeClass('problem-view')
-        cy.nodes('node[properties].personal-value, .risk-solution').forEach(function (node) {
-            const node_props = node.data('properties')
-            if (!(node_props['dc_source'].length ||
-                node_props['schema_academicBook'].length ||
-                node_props['schema_academicSourceNoPaywall'].length ||
-                node_props['schema_academicSourceWithPaywall'].length ||
-                node_props['schema_governmentSource'].length ||
-                node_props['schema_mediaSource'].length ||
-                node_props['schema_mediaSourceForConservatives'].length ||
-                node_props['schema_organizationSource'].length)) {
-                node.addClass('problem-view')
-            }
-        })
-
+        cy.nodes('.node-no-source').addClass('problem-view')
+        cy.nodes('.node-no-sources').addClass('problem-view')
         cy.edges('.edge-no-source').addClass('problem-view')
     },
     apply_personal_values: function () {
@@ -65,92 +49,92 @@ export function make_cyto(personal_value, cyto_storage, storage_index = 0) {
     }
     console.log("Making new graph")
     cy = cytoscape({
-            container: document.getElementById('cytoscape-graph-container'), // container to render in
-            // Moving nodes around will actually change data in cyto_storage.
-            // Cytoscape owns all objects given to it. Maybe we should create a deep copy?
-            elements: cyto_storage[personal_value],
-            style: [ // the stylesheet for the graph
-                {
-                    selector: "edge",
-                    style: {
-                        'width': 2,
-                        'line-color': 'hsl(1, 0%, 60%)',
-                        'target-arrow-shape': 'triangle',
-                        "target-arrow-color": "hsl(1, 0%, 60%)",
-                        'curve-style': 'bezier',
-                        'arrow-scale': 2
-                    }
-                }, {
-                    selector: 'node',
-                    style: {
-                        'label': 'data(label)',
-                        'text-wrap': 'wrap',
-                        // 'text-max-width': '12em', //TODO: check whether this is necessary?
-                        'text-halign': 'center',
-                        'text-valign': 'center',
-                        'width': 'data(__cyto_width)',
-                        'height': 'data(__cyto_height)',
-                        'font-size': 24,
-                        'font-family': 'Segoe UI',
-                        'shape': 'round-rectangle',
-                        'background-color': 'hsl(25,60%,81%)',
-                        'border-width': '2px',
-                        'border-color': 'hsl(18,33%,32%)'
-                    }
-                }, {
-                    selector: '.tree_root',
-                    style: {
-                        'background-color': 'hsl(213,65%,82%)'
-                    }
-                }, {
-                    selector: '.risk-solution',
-                    style: {
-                        'background-color': 'hsl(77,52%,79%)'
-                    }
-                }, {
-                    selector: '.solution-edge',
-                    style: {
-                        "target-arrow-shape": 'none',
-                        "source-arrow-shape": "tee"
-                    }
-                }, {
-                    selector: '.is-highlighted',
-                    style: {
-                        'background-blacken': 0.15
-                    }
-                }, {
-                    selector: "node.filter-view-one",
-                    style: {
-                        'background-blacken': 0.1
-                    }
-                }, {
-                    selector: "edge.filter-view-one",
-                    style: {
-                        'width': 4,
-                        'line-color': 'black',
-                        "target-arrow-color": "black"
-                    }
-                }, {
-                    selector: '.filter-view-two',
-                    style: {
-                        'border-width': '5px',
-                        'border-color': 'hsl(31,92%,23%)',
-                        'background-color': "hsl(154,9%,69%)"
-                    }
-                }, {
-                    selector: '.problem-view',
-                    style: {
-                        'background-blacken': 0.2,
-                        'border-width': '10px',
-                        'border-color': 'hsl(0,86%,25%)',
-                        'line-color': 'red'
-                    }
-                }],
-            layout: {
-                name: 'preset'
-            },
-            wheelSensitivity: 0.15
-        });
+        container: document.getElementById('cytoscape-graph-container'), // container to render in
+        // Moving nodes around will actually change data in cyto_storage.
+        // Cytoscape owns all objects given to it. Maybe we should create a deep copy?
+        elements: cyto_storage[personal_value],
+        style: [ // the stylesheet for the graph
+            {
+                selector: "edge",
+                style: {
+                    'width': 2,
+                    'line-color': 'hsl(1, 0%, 60%)',
+                    'target-arrow-shape': 'triangle',
+                    "target-arrow-color": "hsl(1, 0%, 60%)",
+                    'curve-style': 'bezier',
+                    'arrow-scale': 2
+                }
+            }, {
+                selector: 'node',
+                style: {
+                    'label': 'data(label)',
+                    'text-wrap': 'wrap',
+                    // 'text-max-width': '12em', //TODO: check whether this is necessary?
+                    'text-halign': 'center',
+                    'text-valign': 'center',
+                    'width': 'data(__cyto_width)',
+                    'height': 'data(__cyto_height)',
+                    'font-size': 24,
+                    'font-family': 'Segoe UI',
+                    'shape': 'round-rectangle',
+                    'background-color': 'hsl(25,60%,81%)',
+                    'border-width': '2px',
+                    'border-color': 'hsl(18,33%,32%)'
+                }
+            }, {
+                selector: '.tree_root',
+                style: {
+                    'background-color': 'hsl(213,65%,82%)'
+                }
+            }, {
+                selector: '.risk-solution',
+                style: {
+                    'background-color': 'hsl(77,52%,79%)'
+                }
+            }, {
+                selector: '.solution-edge',
+                style: {
+                    "target-arrow-shape": 'none',
+                    "source-arrow-shape": "tee"
+                }
+            }, {
+                selector: '.is-highlighted',
+                style: {
+                    'background-blacken': 0.15
+                }
+            }, {
+                selector: "node.filter-view-one",
+                style: {
+                    'background-blacken': 0.1
+                }
+            }, {
+                selector: "edge.filter-view-one",
+                style: {
+                    'width': 4,
+                    'line-color': 'black',
+                    "target-arrow-color": "black"
+                }
+            }, {
+                selector: '.filter-view-two',
+                style: {
+                    'border-width': '5px',
+                    'border-color': 'hsl(31,92%,23%)',
+                    'background-color': "hsl(154,9%,69%)"
+                }
+            }, {
+                selector: '.problem-view',
+                style: {
+                    'background-blacken': 0.2,
+                    'border-width': '10px',
+                    'border-color': 'hsl(0,86%,25%)',
+                    'line-color': 'red'
+                }
+            }],
+        layout: {
+            name: 'preset'
+        },
+        wheelSensitivity: 0.15
+    });
 
     let local_storage_key = `cytoscape_graph_cache_${personal_value}_${storage_index}`
     if (localStorage.getItem(local_storage_key) && storage_index) {
@@ -176,7 +160,7 @@ window.filter_exported = filter_exported
 
 function register_cy_callbacks(cy) {
     cy.listen('click', 'edge', function (evt) {
-        document.getElementById('object_output').textContent = JSON.stringify(evt.target.data(), undefined, 2)
+        document.getElementById('object-output').textContent = JSON.stringify(evt.target.data(), undefined, 2)
     })
 
     cy.listen('click', 'node', function (evt) {
--- visualize.py
+++ visualize.py
@@ -5,6 +5,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import networkx as nx
+# from ontology_processing.graph_creation.make_graph import makeGraph
 
 
 def draw_graph(name, graph, output_png=False):
@@ -22,41 +23,28 @@ def draw_graph(name, graph, output_png=False):
     """
     N = nx.nx_agraph.to_agraph(graph)
     N.edge_attr.update(directed=True)
-    N = N.unflatten(f'-f -l6')
+    N = N.unflatten(f"-f -l6")
 
     if output_png:
-        N.layout(prog='dot', args='-Gratio=compress -Grankdir=TB -Gnodesep=0.5 -Gfontsize=20')
+        N.layout(
+            prog="dot", args="-Gratio=compress -Grankdir=TB -Gnodesep=0.5 -Gfontsize=20"
+        )
         N.draw(f"pictures/{name}.png", "png")
 
     # When exporting to JS somehow renders it vertically flipped. So we flip it back.
-    N.layout(prog='dot', args='-Gratio=compress -Grankdir=BT -Gfontsize=25 -Granksep=1 -Gnodesep=1')
+    N.layout(
+        prog="dot",
+        args="-Gratio=compress -Grankdir=BT -Gfontsize=25 -Granksep=1 -Gnodesep=1",
+    )
 
     return N
 
 
-app = dash.Dash(__name__,
-                external_scripts=[{'src': 'assets/make_cyto_graph.mjs', 'type': 'module'}],
-                assets_ignore='.*mjs')
-
-# TODO: update variable naming documentation to match pickle
-# Cyto_data_(no)slns: list of elements that cytoscape.js receives. Contains all info needed to render the graph.
-# edge_data: adjacency dict. Created by edges_to_dict function.
-# graph_data_(no)sln: dict of nx.DiGraph keyed by personal value. Currently unused.
-#       Access by edge_data[personal value string][edge1][edge2] = edge properties (also a dict)
-#       Contains all nodes, including solutions.
-# These two below are currently unused as cytoscape.js graph functions handle everything already.
-# We barely do any calculations in Python.
-# cyto_data_noslns, graph_data_noslns = compute_graphs(G_noslns)
-# edge_data = {personal_value: edges_to_dict(graph) for personal_value, graph in graph_data_slns.items()}
-# Cache in set so we avoid computation
-
-if not os.path.exists("graphs_for_visualization.pickle"):
-    from ontology_processing.process_new_ontology_file import processOntology
-    processOntology('ontology.owl', '.')
-
-# Have to run makegraph function first to get the pickle!
-with open("graphs_for_visualization.pickle", "rb") as f:
-    preprocessed_data = pickle.load(f)
+app = dash.Dash(
+    __name__,
+    external_scripts=[{"src": "assets/make_cyto_graph.mjs", "type": "module"}],
+    assets_ignore=".*mjs",
+)
 
 
 def convert_graph_to_cyto(G, tree_root=None):
@@ -79,140 +67,150 @@ def convert_graph_to_cyto(G, tree_root=None):
     # Use graphviz to layout the graph for us
     N = nx.nx_agraph.to_agraph(G)
     N.edge_attr.update(directed=True)
-    N = N.unflatten('-f -l6')
-    N.layout(prog='dot', args='-Gratio=compress -Grankdir=BT -Gfontsize=25')
+    N = N.unflatten("-f -l6")
+    N.layout(prog="dot", args="-Gratio=compress -Grankdir=BT -Gfontsize=25")
 
     for node in G.nodes:
-        node_label = G.nodes[node]['label']
+        node_label = G.nodes[node]["label"]
         wrapped = textwrap.wrap(node_label, 20)
 
-        G.nodes[node]['label'] = "\n".join(wrapped)
-        G.nodes[node]['__cyto_width'] = f'{len(max(wrapped, key=len)) / 1.5 + 3.5:.2f}em'
-        G.nodes[node]['__cyto_height'] = "{}em".format(len(wrapped) + 3)
+        G.nodes[node]["label"] = "\n".join(wrapped)
+        G.nodes[node][
+            "__cyto_width"
+        ] = f"{len(max(wrapped, key=len)) / 1.5 + 3.5:.2f}em"
+        G.nodes[node]["__cyto_height"] = "{}em".format(len(wrapped) + 3)
 
     N = draw_graph(tree_root, G)
 
-    total_cyto_data = nx.readwrite.json_graph.cytoscape_data(G)['elements']
-    cyto_nodes = total_cyto_data['nodes']
-    cyto_edges = total_cyto_data['edges']
+    total_cyto_data = nx.readwrite.json_graph.cytoscape_data(G)["elements"]
+    cyto_nodes = total_cyto_data["nodes"]
+    cyto_edges = total_cyto_data["edges"]
 
     for edge in cyto_edges:
-        edge['classes'] = G[edge['data']['source']][edge['data']['target']]['cyto_classes']
+        edge["classes"] = G[edge["data"]["source"]][edge["data"]["target"]][
+            "cyto_classes"
+        ]
+        # edge["data"].pop("cyto_classes")
 
     # Extract x y positions using graphviz dot algo. Use x,y positions to make cytoscape graph
     # Also doing some preprocessing
     for node in cyto_nodes:
-        graphviz_node = N.get_node(node['data']['id'])
-        position = graphviz_node.attr.get('pos', []).split(',')
-        node['classes'] = G.nodes[node['data']['id']]['cyto_classes']
-        node['position'] = {'x': int(float(position[0])), 'y': int(float(position[1]))}
+        graphviz_node = N.get_node(node["data"]["id"])
+        position = graphviz_node.attr.get("pos", []).split(",")
+        node["classes"] = node["data"]["cyto_classes"]
+        # node["data"].pop("cyto_classes")
+        node["position"] = {"x": int(float(position[0])), "y": int(float(position[1]))}
 
     return list(cyto_nodes) + list(cyto_edges)
 
 
+
+if not os.path.exists("graphs_for_visualization.pickle"):
+    # This dependency is not listed in the requirements.txt file
+    from ontology_processing.process_new_ontology_file import processOntology
+    processOntology("ontology.owl", ".")
+
+# Have to run makegraph function first to get the pickle!
+with open("graphs_for_visualization.pickle", "rb") as f:
+    preprocessed_data = pickle.load(f)
 total_cyto_data = {}
-if os.environ.get('DEBUG_USE_CACHE'):
-    with open('processed_cyto_data.pickle', 'rb') as f:
+if os.environ.get("DEBUG_USE_CACHE"):
+    with open("processed_cyto_data.pickle", "rb") as f:
         total_cyto_data = pickle.load(f)
 else:
     print("Recreating cyto data")
     for graph in preprocessed_data:
         total_cyto_data[graph] = convert_graph_to_cyto(preprocessed_data[graph], graph)
-    with open('processed_cyto_data.pickle', 'wb') as f:
+    with open("processed_cyto_data.pickle", "wb") as f:
         pickle.dump(total_cyto_data, f)
 
 # Stores the cytograph data so JS code can read it and render it.
 # This data represents all graph data
-cyto_storage = dcc.Store(
-    id='cyto-storage',
-    storage_type='memory',
-    data=total_cyto_data
-)
+cyto_storage = dcc.Store(id="cyto-storage", storage_type="memory", data=total_cyto_data)
 
 layout_parts = {
-    'graph-select': dcc.Dropdown(
-        id='graph-select',
-        className='dropdowns',
-        options=[
-            {'label': name, 'value': name} for name in total_cyto_data.keys()
-        ],
+    "graph-select": dcc.Dropdown(
+        id="graph-select",
+        className="dropdowns",
+        options=[{"label": name, "value": name} for name in total_cyto_data.keys()],
         value="downstream",
-        placeholder="graph type"
+        placeholder="graph type",
     ),
-    'node-search': dcc.Dropdown(
-        id='node-search',
-        className='dropdowns',
-        placeholder="Search for nodes"
+    "node-search": dcc.Dropdown(
+        id="node-search", className="dropdowns", placeholder="Search for nodes"
     ),
-    'right-controls': html.Div(id="right-controls"),
-    'cytoscape-graph-container': html.Div(id="cytoscape-graph-container"),
-    'bottom-controls': html.Div(id='bottom-controls')
+    "right-controls": html.Div(id="right-controls"),
+    "cytoscape-graph-container": html.Div(id="cytoscape-graph-container"),
+    "bottom-controls": html.Div(id="bottom-controls"),
 }
 
-app.layout = html.Div([
-    cyto_storage,
-    html.Div(children=list(layout_parts.values()), className='grid-container'),
-    html.Div(id='dummy1'), html.Div(id='dummy2'), html.Div(id='dummy3'), html.Div(id='dummy4'), html.Div(id='dummy5')
-])
+app.layout = html.Div(
+    [
+        cyto_storage,
+        html.Div(children=list(layout_parts.values()), className="grid-container"),
+        html.Div(id="dummy1"),
+        html.Div(id="dummy2"),
+        html.Div(id="dummy3"),
+        html.Div(id="dummy4"),
+        html.Div(id="dummy5"),
+    ]
+)
 
 # Runs on load to fill html.Div element output with custom HTMl tags
 # Not using Dash HTML library because much more complicated than writing plain HTML.
 app.clientside_callback(
     dash.dependencies.ClientsideFunction(
         # Function is located in file assets/callbc.js
-        namespace='clientside',
-        function_name='fill_output_div'
+        namespace="clientside",
+        function_name="fill_output_div",
     ),
-    dash.dependencies.Output('dummy2', 'children'),
-    dash.dependencies.Input('dummy1', 'children')
+    dash.dependencies.Output("dummy2", "children"),
+    dash.dependencies.Input("dummy1", "children"),
 )
 
 # Updates the personal values in the dropdown.
 app.clientside_callback(
     dash.dependencies.ClientsideFunction(
-        namespace='clientside',
-        function_name='update_personal_value'
+        namespace="clientside", function_name="update_personal_value"
     ),
-    dash.dependencies.Output('dummy3', 'children'),
-    [dash.dependencies.Input('graph-select', 'value')],
+    dash.dependencies.Output("dummy3", "children"),
+    [dash.dependencies.Input("graph-select", "value")],
 )
 
 # Copies our Python cytoscape_data to Javascript.
 # This contains all the data needed to render all graphs.
 app.clientside_callback(
     dash.dependencies.ClientsideFunction(
-        namespace='clientside',
-        function_name='update_storage_globals'
+        namespace="clientside", function_name="update_storage_globals"
     ),
-    dash.dependencies.Output('dummy1', 'children'),
-    [dash.dependencies.Input('cyto-storage', 'data')],
+    dash.dependencies.Output("dummy1", "children"),
+    [dash.dependencies.Input("cyto-storage", "data")],
 )
 
 # Copies our Python cytoscape_data to Javascript.
 # This contains all the data needed to render all graphs.
 app.clientside_callback(
     dash.dependencies.ClientsideFunction(
-        namespace='clientside',
-        function_name='search_nodes'
+        namespace="clientside", function_name="search_nodes"
     ),
-    dash.dependencies.Output('dummy5', 'children'),
-    [dash.dependencies.Input('node-search', 'value')],
+    dash.dependencies.Output("dummy5", "children"),
+    [dash.dependencies.Input("node-search", "value")],
 )
 
 
 @app.callback(
-    dash.dependencies.Output('node-search', 'options'),
-    dash.dependencies.Input('graph-select', 'value')
+    dash.dependencies.Output("node-search", "options"),
+    dash.dependencies.Input("graph-select", "value"),
 )
 def test(input_value):
     if input_value in preprocessed_data:
         nodes = sorted(preprocessed_data[input_value].nodes())
-    elif input_value in preprocessed_data['personal_value_slns']:
-        nodes = sorted(preprocessed_data['personal_value_slns'][input_value])
+    elif input_value in preprocessed_data["personal_value_slns"]:
+        nodes = sorted(preprocessed_data["personal_value_slns"][input_value])
     else:
         print("Invalid input value ", input_value)
         return
-    return [{'label': node, 'value': node} for node in nodes]
+    return [{"label": node, "value": node} for node in nodes]
 
 
-app.run_server(debug=True)
+app.run_server('0.0.0.0', debug=False)
